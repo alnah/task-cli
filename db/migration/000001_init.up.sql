857d2e70a93b1294c4416a88c2d7f91f438c78d0
alnah <alexis.nahan@gmail.com>
2024-10-25 19:11:36 -0300

    feat(db): add init migration files

---
+++ db/migration/000001_init.up.sql
@@ -0,0 +1,16 @@
+CREATE TYPE "status" AS ENUM (
+  'todo',
+  'in-progress',
+  'done'
+);
+
+CREATE TABLE "task" (
+  "id" serial PRIMARY KEY,
+  "description" varchar(100) NOT NULL,
+  "status" status DEFAULT 'todo',
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT null,
+  "deleted_at" timestamptz DEFAULT null
+);
+
+CREATE INDEX ON "task" ("status");
